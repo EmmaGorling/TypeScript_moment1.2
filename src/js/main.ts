ab04bde3a64b43c3d675c0037ea12eb02c1a6162
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-03-21 19:15:26 +0100

    Får felindikationer på allt jag försöker göra... Kommer göra en ny TS-fil och testa annat...

--- src/js/main.ts
+++ src/js/main.ts
@@ -15,20 +15,60 @@ enum Progression {
 window.onload = () => init();
 
 function init(): void {
-    //Hämta element från index.HTML
-    const codeInput: HTMLElement = document.getElementById('courseCode');
-    const nameInput: HTMLElement = document.getElementById('courseName');
-    const progInput: HTMLElement = document.getElementById('courseProg');
-    const urlInput: HTMLElement = document.getElementById('courseUrl');
-    const addBtn: HTMLElement = document.getElementById('addBtn');
+    
+    const addBtn: HTMLElement|null = document.getElementById('addBtn');
 
     // Eventlyssnare på knapp
     addBtn.addEventListener('click', () => {
-        addCourse();
+        getInfo();
     })
 
 };
-
+/*
 function addCourse(): void {
-    console.log('Hej');
+    //Hämta element från index.HTML
+    const codeInput: HTMLElement|null = document.getElementById('courseCode') ;
+    const nameInput: HTMLElement|null = document.getElementById('courseName');
+    const progInput: HTMLElement|null = document.getElementById('courseProg');
+    const urlInput: HTMLElement|null = document.getElementById('courseUrl');
+    const tBody: HTMLElement|null = document.getElementById('tBody');
+
+    // Skapa table-rad
+    const trEl: HTMLTableRowElement = document.createElement('tr');
+
+    // Skapa td med kurskod
+    const codeTd: HTMLTableCellElement = document.createElement('td');
+    let codeText = document.createTextNode(codeInput.value);
+    codeTd.appendChild(codeText);
+
+
+    // Lägg till celler i tr
+    trEl.appendChild(codeTd);
+
+    // Lägg till tr i tbody
+    tBody?.appendChild(trEl);
 }
+*/
+function getInfo():void {
+    //Hämta element från index.html
+    const codeInput: HTMLElement|null = document.getElementById('courseCode') ;
+    const nameInput: HTMLElement|null = document.getElementById('courseName');
+    const progInput: HTMLElement|null = document.getElementById('courseProg');
+    const urlInput: HTMLElement|null = document.getElementById('courseUrl');
+    const tBody: HTMLElement|null = document.getElementById('tBody');
+
+    if(progInput.value === 'A' || progInput.value === 'B' || progInput.value === 'C') {
+        let course: Course = {
+            code: codeInput.value,
+            name: nameInput.value,
+            progression: progInput.value,
+            syllabus: urlInput.value
+        }
+
+        console.log(course);
+    } else {
+        let errorDiv: HTMLDivElement | null = document.getElementById('error');
+        errorDiv.innerHTML = '<p>Progressionen måste vara A, B eller C.'
+    }
+    
+};
