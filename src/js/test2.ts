3ab8e0e5ba6418748024e4325db2b1fbe9b5b665
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-03-23 00:49:54 +0100

    Jag hoppar av, programmering är uppenbarligen inte min grej

--- src/js/test2.ts
+++ src/js/test2.ts
@@ -1,66 +1,37 @@
-// Interface för kurser
-interface Course {
-    code: string;
-    name: string;
-    progression: string;
-    syllabus: string;
-}
+//Hämta element från index.html
+const codeInput = document.getElementById('courseCode') as HTMLInputElement;
+const nameInput = document.getElementById('courseName') as HTMLInputElement;
+const progInput = document.getElementById('courseProg') as HTMLInputElement;
+const urlInput = document.getElementById('courseUrl') as HTMLInputElement;
 
 window.onload = init;
 
-function init(): void {
-    
+function init():void {
     const addBtn = document.getElementById('addBtn') as HTMLButtonElement;
 
     // Eventlyssnare på knapp
     addBtn.addEventListener('click', () => {
         getInfo();
     })
+}
 
-};
-
-// Hämta från inputs
 function getInfo():void {
-    //Hämta element från index.html
-    const codeInput = document.getElementById('courseCode') as HTMLInputElement;
-    const nameInput = document.getElementById('courseName') as HTMLInputElement;
-    const progInput = document.getElementById('courseProg') as HTMLInputElement;
-    const syllInput = document.getElementById('courseSyll') as HTMLInputElement;
-
-    // Lägg ttil värden i nytt objekt
-    let newCourse: Course = {
-        code: codeInput.value,
-        name: nameInput.value,
-        progression: progInput.value,
-        syllabus: syllInput.value
+    let newCourse = {
+        code:codeInput.value
     };
-    
-    // Anropa viewCourse
-    viewCourse(newCourse, codeInput);
+
+    writeToTable(newCourse);
 };
 
-function viewCourse(course:any, codeInput: HTMLInputElement): void {
-    const tBody = document.getElementById('tBody') as HTMLInputElement;
+function writeToTable(course) {
 
-    const errorDiv = document.getElementById('error') as HTMLInputElement;
+    const tBody = document.getElementById('tBody') as HTMLInputElement;
 
     tBody.innerHTML += `
     <tr class='courseTr'>
-        <td class='tdCode'>${course.code}</td>
-        <td class='tdName'>${course.name}</td>
-        <td class='tdProg'>${course.progression}</td>
-        <td class='tdSyll'>${course.syllabus}</td>
+        <td class='tdCode>${course.code}</td>
     </tr>`
 
-    storeCourses(course);
-}
-
-function storeCourses(course): void {
-
-    let courseTr = document.getElementsByClassName('courseTr');
-    let tdCode = document.getElementsByClassName('tdCode');
-
-    console.log(tdCode.data);
-
+    //storeCourses();
 }
 
