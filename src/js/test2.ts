ee5c79f398f9e9a97e4c87fcba601adf1aa72437
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-03-24 08:51:09 +0100

    Börjar om

--- src/js/test2.ts
+++ src/js/test2.ts
@@ -4,34 +4,5 @@ const nameInput = document.getElementById('courseName') as HTMLInputElement;
 const progInput = document.getElementById('courseProg') as HTMLInputElement;
 const urlInput = document.getElementById('courseUrl') as HTMLInputElement;
 
-window.onload = init;
 
-function init():void {
-    const addBtn = document.getElementById('addBtn') as HTMLButtonElement;
-
-    // Eventlyssnare på knapp
-    addBtn.addEventListener('click', () => {
-        getInfo();
-    })
-}
-
-function getInfo():void {
-    let newCourse = {
-        code:codeInput.value
-    };
-
-    writeToTable(newCourse);
-};
-
-function writeToTable(course) {
-
-    const tBody = document.getElementById('tBody') as HTMLInputElement;
-
-    tBody.innerHTML += `
-    <tr class='courseTr'>
-        <td class='tdCode>${course.code}</td>
-    </tr>`
-
-    //storeCourses();
-}
 
